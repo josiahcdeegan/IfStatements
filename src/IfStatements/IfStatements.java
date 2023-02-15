25309981b29bb95be9d274dbc319f6a942db6342
josiahcdeegan <114314800+josiahcdeegan@users.noreply.github.com>
2023-02-15 18:54:36 -0500

    HelloThere

---
+++ src/IfStatements/IfStatements.java
@@ -0,0 +1,34 @@
+package IfStatements;
+
+import java.util.Scanner;
+
+public class IfStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+if(s .equals("1")) { 
+
+	  System.out.println("I can buy 1 hamburger for lunch!"); 
+
+	} 
+
+	else if(s .equals("2")) { 
+		  System.out.println("I can buy 2 hamburgers for lunch!"); 
+	} 
+	else if(s .equals("3")) { 
+		  System.out.println("I can't buy 3 hamburgers for lunch!"); 
+	} 
+	else { 
+
+	  System.out.println("Nuts! I can't buy any hamburgers!"); 
+
+	} 
+
+	}
+
+}
+//== is considered an operator in Java, is mostly used to compare the reference values and objects, 
+//We can use it with objects and primitives, 
